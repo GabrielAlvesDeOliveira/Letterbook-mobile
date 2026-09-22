c53fc05b5b28633af2686aa112fb95929e193a6b
Gabriel Alves <alvesg2003@gmail.com>
2026-09-22 18:36:13 -0300

    Adiciona Entrega 2: listagem de livros com ListView e Adapter customizado
    - Cria a entidade Livro (titulo, autor, genero, anoPublicacao)
    - Adiciona arrays.xml com dados de 10 livros (4 tipos de informacao)
    - Cria ListaLivrosActivity com ListView + LivroAdapter customizado
    - Toast ao clicar em um item, identificando o livro
    - ListaLivrosActivity vira a nova tela Launcher (MAIN/LAUNCHER)
    - MainActivity da Entrega 1 permanece no projeto, sem intent-filter de launcher
    - Aplica a paleta de cores do site (variaveis.css) em colors.xml para manter a mesma identidade visual

---
+++ app/src/main/java/br/edu/utfpr/alunos/gabrielalves/letterbook/ListaLivrosActivity.java
@@ -0,0 +1,46 @@
+package br.edu.utfpr.alunos.gabrielalves.letterbook;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ListaLivrosActivity extends AppCompatActivity {
+
+    private ArrayList<Livro> livros;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_livros);
+
+        livros = carregarLivros();
+
+        ListView listViewLivros = findViewById(R.id.list_view_livros);
+        LivroAdapter adapter = new LivroAdapter(this, livros);
+        listViewLivros.setAdapter(adapter);
+
+        listViewLivros.setOnItemClickListener((parent, view, position, id) -> {
+            Livro livroClicado = livros.get(position);
+            String mensagem = livroClicado.getTitulo() + " - " + livroClicado.getAutor()
+                    + " (" + livroClicado.getAnoPublicacao() + ")";
+            Toast.makeText(ListaLivrosActivity.this, mensagem, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private ArrayList<Livro> carregarLivros() {
+        String[] titulos = getResources().getStringArray(R.array.titulos_livros);
+        String[] autores = getResources().getStringArray(R.array.autores_livros);
+        String[] generos = getResources().getStringArray(R.array.generos_livros);
+        String[] anos = getResources().getStringArray(R.array.anos_livros);
+
+        ArrayList<Livro> lista = new ArrayList<>();
+        for (int i = 0; i < titulos.length; i++) {
+            lista.add(new Livro(titulos[i], autores[i], generos[i], Integer.parseInt(anos[i])));
+        }
+        return lista;
+    }
+}
---
+++ app/src/main/java/br/edu/utfpr/alunos/gabrielalves/letterbook/LivroAdapter.java
@@ -0,0 +1,39 @@
+package br.edu.utfpr.alunos.gabrielalves.letterbook;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class LivroAdapter extends ArrayAdapter<Livro> {
+
+    public LivroAdapter(Context context, ArrayList<Livro> livros) {
+        super(context, 0, livros);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.item_livro, parent, false);
+        }
+
+        Livro livro = getItem(position);
+
+        TextView textTitulo = convertView.findViewById(R.id.text_titulo);
+        TextView textAutor = convertView.findViewById(R.id.text_autor);
+        TextView textGenero = convertView.findViewById(R.id.text_genero);
+        TextView textAno = convertView.findViewById(R.id.text_ano);
+
+        textTitulo.setText(livro.getTitulo());
+        textAutor.setText(livro.getAutor());
+        textGenero.setText(livro.getGenero());
+        textAno.setText(String.valueOf(livro.getAnoPublicacao()));
+
+        return convertView;
+    }
+}
